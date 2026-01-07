5ac83bd1ada193bc61df2cce338cf8cb6858c998
LCL262 <24037880@myrp.edu.sg>
2026-01-07 12:19:49 +0800

    1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //include the required packages
 const express = require('express');
-const mysql = requrie('mysql2/promise');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 const port = 3000;
 
