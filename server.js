a2fdf464022f707f1008d88c998e4c63ca4909da
LCL262 <24037880@myrp.edu.sg>
2026-01-07 12:09:20 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+//include the required packages
+const express = require('express');
+const mysql = requrie('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+//initialize Express App
+const app = express();
+//helps app to read json
+app.use(express.json());
+
+//Start the server
+app.listen(port, () => {console.log('Listening on port: ', port);});
+
+//Example route: get all cards
+app.get('/allcards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM edfaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error for all cards'});
+    }
+});
