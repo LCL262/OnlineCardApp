b268dc9b162ac9a550c28f4dea4b214530d1a626
LCL262 <24037880@myrp.edu.sg>
2026-01-27 13:21:07 +0800

    4

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ const cors = require("cors");
 
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://c219-card-app-41x4.onrender.com/",
-    "https://c219-card-app-4.onrender.com/"
+    "https://c219-card-app-41x4.onrender.com",
+    "https://c219-card-app-4.onrender.com"
 ];
 
 app.use(
